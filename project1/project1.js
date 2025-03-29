5bd1ce5b850a2327b3ea1e406f374973c5698a86
Abhi <154977045+BcaAbhishek@users.noreply.github.com>
2025-03-29 10:46:16 +0530

    Add files via upload

---
+++ project1/project1.js
@@ -0,0 +1,39 @@
+// // let navbar = document.querySelector('.navbar');
+
+// // document.querySelector('#menu-btn').onclick = () =>{
+// // 	navbar.classList.toggle('active');
+// // 	searchForm.classList.remove('active');
+// // 	cartitem.classList.remove('active');
+// // }
+
+// // let searchForm = document.querySelector('.search-form');
+
+// // document.querySelector('#search-btn').onclick = () =>{
+// // 	searchForm.classList.toggle('active');
+// // 	navbar.classList.remove('active');
+// // 	cartitem.classList.remove('active');
+// // }
+
+
+// // let cartitem = document.querySelector('.cart-item-container');
+
+// // document.querySelector('#cart-btn').onclick = () =>{
+// // 	cartitem.classList.toggle('active');
+// // 	navbar.classList.remove('active');
+// // 	searchForm.classList.remove('active');
+// // }
+
+// // window.onscroll = () =>{
+// // 	navbar.classList.remove('active');
+// // 	searchForm.classList.remove('active');
+// // 	cartitem.classList.remove('active');
+// // }
+
+// document.addEventListener("home",function click(){
+// 	console.log("clicked");
+// 	alert(hello);
+// })
+
+function hello(){
+	console.log("meow");
+}
